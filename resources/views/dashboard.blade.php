d337a48dcfa2713a67449eb0a85be07ef3fcc6fa
umarhadimukti <umarhadimukti@gmail.com>
2024-05-23 11:51:37 +0700

    feat: 🎸 add new dashboard ui

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,9 +1,167 @@
-@extends('layouts.auth')
+@extends('layouts.dashboard')
 
 @section('title', 'Dashboard')
 
 @section('container')
-    <div class="container-dashboard py-1">
-        <h3>Dashboard </h3>
+    <div class="container-dashboard py-5 px-3">
+        <div class="hero-container">
+            <img src="{{ asset('./img/dashboard.svg') }}" alt="hero">
+        </div>
+
+        <div class="wrapper px-2 mt-3 pt-4 pb-5" style="background-color: white; width: 80%; margin: 0px auto; border-radius: 15px;">
+            <h5 style="font-size: 14px; padding: 6px 15px; border-radius: 20px; color: rgb(18, 33, 160); background-color: rgb(233, 231, 255); border: 1px solid rgb(218, 218, 218); width: max-content; margin-bottom: 25px;">Informasi Data Karyawan</h5>
+
+            <div style="display: flex; flex-wrap: wrap; gap: 13px; background-color: white; width: 100%; margin: 0px auto; border-radius: 15px;">
+                <div class="karyawan-organik py-2" style="width: 24%;">
+                    <div class="w-75 mb-2" style="margin: 0px auto; border-bottom: 2px solid rgb(236, 235, 235);">
+                        <h5 class="text-center mt-1" style="font-size: 15px;">Karyawan Organik</h5>
+                    </div>
+                    <div class="d-flex flex-column">
+                        <div class="d-flex justify-content-center gap-2 mt-3 mb-2">
+                            <div class="">
+                                <img src="{{ asset('./img/employees.svg') }}" alt="">
+                            </div>
+                            <div class="d-flex flex-column">
+                                <h5 style="font-size: 14px;">Total Employee</h5>
+                                <p class="text-center" style="font-size: 30px; font-weight: bold;">100</p>
+                            </div>
+                        </div>
+                        <div class="d-flex justify-content-center gap-3">
+                            <div class="d-flex gap-2 mt-3">
+                                <div class="">
+                                    <img src="{{ asset('./img/male.svg') }}" alt="">
+                                </div>
+                                <div class="d-flex flex-column">
+                                    <h5 style="font-size: 14px;">Male</h5>
+                                    <p class="text-center" style="font-size: 20px; font-weight: bold;">100</p>
+                                </div>
+                            </div>
+                            <div class="d-flex gap-2 mt-3">
+                                <div class="">
+                                    <img src="{{ asset('./img/female.svg') }}" alt="">
+                                </div>
+                                <div class="d-flex flex-column">
+                                    <h5 style="font-size: 14px;">Female</h5>
+                                    <p class="text-center" style="font-size: 20px; font-weight: bold;">100</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+    
+                <div class="turnover-organik py-2" style="width: 24%;">
+                    <div class="w-75 mb-2" style="margin: 0px auto; border-bottom: 2px solid rgb(236, 235, 235);">
+                        <h5 class="text-center mt-1" style="font-size: 15px;">TurnOver Organik</h5>
+                    </div>
+                    <div class="d-flex flex-column">
+                        <div class="d-flex justify-content-center gap-2 mt-3 mb-2">
+                            <div class="">
+                                <img src="{{ asset('./img/employees.svg') }}" alt="">
+                            </div>
+                            <div class="d-flex flex-column">
+                                <h5 style="font-size: 14px;">Total Employee</h5>
+                                <p class="text-center" style="font-size: 30px; font-weight: bold;">100</p>
+                            </div>
+                        </div>
+                        <div class="d-flex justify-content-center gap-3">
+                            <div class="d-flex gap-2 mt-3">
+                                <div class="">
+                                    <img src="{{ asset('./img/male.svg') }}" alt="">
+                                </div>
+                                <div class="d-flex flex-column">
+                                    <h5 style="font-size: 14px;">Male</h5>
+                                    <p class="text-center" style="font-size: 20px; font-weight: bold;">100</p>
+                                </div>
+                            </div>
+                            <div class="d-flex gap-2 mt-3">
+                                <div class="">
+                                    <img src="{{ asset('./img/female.svg') }}" alt="">
+                                </div>
+                                <div class="d-flex flex-column">
+                                    <h5 style="font-size: 14px;">Female</h5>
+                                    <p class="text-center" style="font-size: 20px; font-weight: bold;">100</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+    
+                <div class="turnover-tpp py-2" style="width: 24%;">
+                    <div class="w-75 mb-2" style="margin: 0px auto; border-bottom: 2px solid rgb(236, 235, 235);">
+                        <h5 class="text-center mt-1" style="font-size: 15px;">TurnOver TPP</h5>
+                    </div>
+                    <div class="d-flex flex-column">
+                        <div class="d-flex justify-content-center gap-2 mt-3 mb-2">
+                            <div class="">
+                                <img src="{{ asset('./img/employees.svg') }}" alt="">
+                            </div>
+                            <div class="d-flex flex-column">
+                                <h5 style="font-size: 14px;">Total Employee</h5>
+                                <p class="text-center" style="font-size: 30px; font-weight: bold;">100</p>
+                            </div>
+                        </div>
+                        <div class="d-flex justify-content-center gap-3">
+                            <div class="d-flex gap-2 mt-3">
+                                <div class="">
+                                    <img src="{{ asset('./img/male.svg') }}" alt="">
+                                </div>
+                                <div class="d-flex flex-column">
+                                    <h5 style="font-size: 14px;">Male</h5>
+                                    <p class="text-center" style="font-size: 20px; font-weight: bold;">100</p>
+                                </div>
+                            </div>
+                            <div class="d-flex gap-2 mt-3">
+                                <div class="">
+                                    <img src="{{ asset('./img/female.svg') }}" alt="">
+                                </div>
+                                <div class="d-flex flex-column">
+                                    <h5 style="font-size: 14px;">Female</h5>
+                                    <p class="text-center" style="font-size: 20px; font-weight: bold;">100</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+    
+                <div class="data-plt py-2" style="width: 24%;">
+                    <div class="w-75 mb-2" style="margin: 0px auto; border-bottom: 2px solid rgb(236, 235, 235);">
+                        <h5 class="text-center mt-1" style="font-size: 15px;">Data PLT</h5>
+                    </div>
+                    <div class="d-flex flex-column">
+                        <div class="d-flex justify-content-center gap-2 mt-3 mb-2">
+                            <div class="">
+                                <img src="{{ asset('./img/employees.svg') }}" alt="">
+                            </div>
+                            <div class="d-flex flex-column">
+                                <h5 style="font-size: 14px;">Total Employee</h5>
+                                <p class="text-center" style="font-size: 30px; font-weight: bold;">100</p>
+                            </div>
+                        </div>
+                        <div class="d-flex justify-content-center gap-3">
+                            <div class="d-flex gap-2 mt-3">
+                                <div class="">
+                                    <img src="{{ asset('./img/male.svg') }}" alt="">
+                                </div>
+                                <div class="d-flex flex-column">
+                                    <h5 style="font-size: 14px;">Male</h5>
+                                    <p class="text-center" style="font-size: 20px; font-weight: bold;">100</p>
+                                </div>
+                            </div>
+                            <div class="d-flex gap-2 mt-3">
+                                <div class="">
+                                    <img src="{{ asset('./img/female.svg') }}" alt="">
+                                </div>
+                                <div class="d-flex flex-column">
+                                    <h5 style="font-size: 14px;">Female</h5>
+                                    <p class="text-center" style="font-size: 20px; font-weight: bold;">100</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
     </div>
 @endsection
---
+++ resources/views/partials/navbar-dashboard.blade.php
@@ -0,0 +1,58 @@
+<nav class="navbar navbar-dashboard navbar-expand-lg bg-body-tertiary py-3" style="backdrop-filter: blur(10px); -webkit-backdrop-filter: blur(10px); box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1); background-color: rgba(255, 255, 255, 0.7);">
+    <div class="container d-flex justify-content-between" style="width: 90%; margin: 0px auto">
+      <a class="navbar-brand d-flex gap-2" style="width: 25%" href="{{ route('admin.dashboard') }}">
+        <img src="{{ asset('./img/angkasapura.svg') }}" alt="angkasapura">
+        <img src="{{ asset('./img/acd.svg') }}" alt="acd">
+      </a>
+
+      <div class="links d-flex gap-2 align-items-center">
+        <a href="{{ route('data.karyawanorganik.karyawan.index') }}">
+            Karyawan Organik
+        </a>
+        <a href="{{ route('data.turnoverorganik.karyawan.index') }}">
+            TurnOver Organik
+        </a>
+        <a href="{{ route('data.turnovertpp.karyawan.index') }}" class="text-center" style="width: 180px;">
+            TurnOver TPP
+        </a>
+        <a href="{{ route('data.dataplt.karyawan.index') }}" class="text-center" style="width: 180px;">
+            Data TLP
+        </a>
+      </div>
+
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarNav">
+        <ul class="navbar-nav">
+          @auth
+            <li class="nav-item dropdown">
+                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    {{ Auth::user()->name }}
+                </a>
+                <ul class="dropdown-menu dropdown-menu-end rounded px-2 py-3" style="padding: 0px; border: none; outline: none;" aria-labelledby="navbarDropdown">
+                    <li>
+                        <a class="dropdown-item d-flex align-items-center gap-1" href="{{ route('logout') }}">
+                          <svg width="17" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5">
+                            <path fill-rule="evenodd" d="M3 4.25A2.25 2.25 0 0 1 5.25 2h5.5A2.25 2.25 0 0 1 13 4.25v2a.75.75 0 0 1-1.5 0v-2a.75.75 0 0 0-.75-.75h-5.5a.75.75 0 0 0-.75.75v11.5c0 .414.336.75.75.75h5.5a.75.75 0 0 0 .75-.75v-2a.75.75 0 0 1 1.5 0v2A2.25 2.25 0 0 1 10.75 18h-5.5A2.25 2.25 0 0 1 3 15.75V4.25Z" clip-rule="evenodd" />
+                            <path fill-rule="evenodd" d="M6 10a.75.75 0 0 1 .75-.75h9.546l-1.048-.943a.75.75 0 1 1 1.004-1.114l2.5 2.25a.75.75 0 0 1 0 1.114l-2.5 2.25a.75.75 0 1 1-1.004-1.114l1.048-.943H6.75A.75.75 0 0 1 6 10Z" clip-rule="evenodd" />
+                          </svg>
+                            
+                          Logout
+                        </a>
+                    </li>
+                </ul>
+            </li>
+          @endauth
+          @guest
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('login') }}">Login</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('register') }}">Register</a>
+            </li>
+          @endguest
+        </ul>
+      </div>
+    </div>
+  </nav>
