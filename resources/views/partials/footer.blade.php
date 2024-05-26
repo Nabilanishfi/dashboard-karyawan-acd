957d63df876a9e88cff7f3502cce9b9e50100967
umarhadimukti <umarhadimukti@gmail.com>
2024-05-26 09:34:34 +0700

    fix: 🐛 add copyright 2024

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,12 +1,12 @@
-<div class="py-5 d-flex justify-content-center">
-    <div class="py-5 d-flex justify-content-between " style="width: 85%;">
+<div class="pt-5 pb-3 d-flex align-items-center flex-column gap-4">
+    <div class="py-5 d-flex justify-content-between" style="width: 85%; border-bottom: 1px solid rgb(207, 207, 207);">
         <div class="d-flex flex-column gap-1" style="width: 48%">
             <div class="">
-                <h5>Angkasa Pura II - INDONESIA'S AIRPORT COMPANY</h5>
+                <h6 class="fw-bold">Angkasa Pura II - INDONESIA'S AIRPORT COMPANY</h6>
                 <p>VMF4+XF8, Soekarno-Hatta International Airport Building, RT.001/RW.010, 600, Pajang, Kec. Benda, Kota Tangerang, Banten 15126</p>
             </div>
             <div class="">
-                <h5>Airport Construction Division</h5>
+                <h6 class="fw-bold">Airport Construction Division</h6>
                 <p>Bandar Udara Internasional Soekarno-Hatta Terminal 3, RT.001/RW.010, Pajang, Kec. Benda, Kota Tangerang, Banten 15126</p>
             </div>
         </div>
@@ -18,5 +18,8 @@
                 <img src="{{ asset('./img/acd.svg') }}" alt="acd">
             </div>
         </div>
+    </div>
+    <div class="d-flex justify-content-center text-center">
+        <p>&copy; 2024 Airport Construction Division.</p>
     </div>
 </div>
