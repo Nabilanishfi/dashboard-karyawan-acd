757670fa3b6cfb74332d99ac860fa3772e5440af
umarhadimukti <umarhadimukti@gmail.com>
2024-05-29 09:22:53 +0700

    fix: 🐛 seeding jabatan_existings

--- database/seeders/ExistingSeeder.php
+++ database/seeders/ExistingSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Jabatan\Existing;
 
 class ExistingSeeder extends Seeder
 {
@@ -14,6 +14,95 @@ class ExistingSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Existing::create([
+            'name' => 'Manager of Project Non CGK',
+        ]);
+        Existing::create([
+            'name' => 'Manager of PIU Integrated Building',
+        ]);
+        Existing::create([
+            'name' => 'Manager of PIU Hotel',
+        ]);
+        Existing::create([
+            'name' => 'Manager of PIU PDG',
+        ]);
+        Existing::create([
+            'name' => 'Manager of PIU PGK',
+        ]);
+        Existing::create([
+            'name' => 'Manager of PIU PNK',
+        ]);
+        Existing::create([
+            'name' => 'PLT Manager PIU BDO PWL',
+        ]);
+        Existing::create([
+            'name' => 'Manager of Legal Contract',
+        ]);
+        Existing::create([
+            'name' => 'Manager of Treasury',
+        ]);
+        Existing::create([
+            'name' => 'Manager of Project CGK',
+        ]);
+        Existing::create([
+            'name' => 'Manager of Project Management',
+        ]);
+        Existing::create([
+            'name' => 'Manager of Project Evaluation - Non CGK',
+        ]);
+        Existing::create([
+            'name' => 'EGM KC BANDARA BESAR JENDERAL SOEDIRMAN',
+        ]);
+        Existing::create([
+            'name' => 'Deputy Egm Of Project Implementation',
+        ]);
+        Existing::create([
+            'name' => 'Manager of Project Evaluation - CGK',
+        ]);
+        Existing::create([
+            'name' => 'Manager Bugeting & Accounting',
+        ]);
+        Existing::create([
+            'name' => 'Senior General Manager of Supply Center (KP)',
+        ]);
+        Existing::create([
+            'name' => 'SM of Operation & Engineering Supply (KP)',
+        ]);
+        Existing::create([
+            'name' => 'General Affairs & Adm. Senior Officer',
+        ]);
+        Existing::create([
+            'name' => 'Manager of Airport Maintenance KC. Bandara SMB II',
+        ]);
+        Existing::create([
+            'name' => 'SM of Project Financial & GA',
+        ]);
+        Existing::create([
+            'name' => 'SM of Project Procurement & Legal Contract',
+        ]);
+        Existing::create([
+            'name' => 'Project Non CGK - Senior Officer',
+        ]);
+        Existing::create([
+            'name' => 'SM of Business & Supporting Supply',
+        ]);
+        Existing::create([
+            'name' => 'Deputy of EGM Project Implementation ACD',
+        ]);
+        Existing::create([
+            'name' => 'Project Evaluation Senior Officer - Non CGK',
+        ]);
+        Existing::create([
+            'name' => 'Manager of Project - CGK',
+        ]);
+        Existing::create([
+            'name' => 'Manager of Project - Non CGK',
+        ]);
+        Existing::create([
+            'name' => 'Manager of Budgeting & Accounting',
+        ]);
+        Existing::create([
+            'name' => 'Manager Of PIU Integrated Building ',
+        ]);
     }
 }
